97ad3fd0f81fce5f9aa848eed281f6f3b7b08dfd
elderz6 <Padrao@NTHTCPS001755.hildebra.intra>
2018-07-13 10:29:38 -0300

    ready to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 //api route setup
 const items = require('./routes/api/items');
@@ -19,6 +20,17 @@ mongoose.connect(db)
   .catch(err => console.log(err));
 
 app.use('/api/items', items);
+
+//load static assets in production
+if (proccess.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+  {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
